a10a64d8707c7d5cae1a71f251e15090482a794f
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-11-16 18:48:13 +0000

    /'

--- led8x8.py
+++ led8x8.py
@@ -1,12 +1,9 @@
-# LEDdisplay class
-import time
+
 from shifter import Shifter    # extend by composition
-import random
 
-class Led8x8():
+class led8x8():
 
-  #'Class for controlling a 7-segment LED display'
-  numbers = [  #sequence for different positions
+  numbers = [ 
     0b11111110, # 0 
     0b11111101, # 1
     0b11111011, # 2
@@ -16,11 +13,10 @@ class Led8x8():
     0b10111111, # 6
     0b01111111] # 7
 
-  def __init__(self, data, latch, clock): #make the definition off of the shifter class
+  def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
  
   def display(self, x, y):
-       # change this value to pick which row the pattern appears on
-    self.shifter.shiftByte(Led8x8.numbers[y])  # load the row values
+    self.shifter.shiftByte(led8x8.numbers[y])  
     self.shifter.shiftByte(1 << (x-1))   # select the given row
       
