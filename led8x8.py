941426e943ef0f01fcabf426161415eb6ae87f53
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-11-16 18:57:06 +0000

    asd

--- led8x8.py
+++ led8x8.py
@@ -3,20 +3,20 @@ from shifter import Shifter    # extend by composition
 
 class led():
 
-  numbers = [ 
-    0b11111110, # 0 
-    0b11111101, # 1
-    0b11111011, # 2
-    0b11110111, # 3
-    0b11101111, # 4
-    0b11011111, # 5
-    0b10111111, # 6
-    0b01111111] # 7
+  pat = [
+    0b01111111,
+    0b10111111,
+    0b11011111,
+    0b11101111,
+    0b11110111,
+    0b11111011,
+    0b11111101,
+    0b11111110]
 
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
  
   def display(self, r, c):
-    self.shifter.shiftByte(led.numbers[r])  
+    self.shifter.shiftByte(led.pat[r])  
     self.shifter.shiftByte(1 << (c-1))   
       
