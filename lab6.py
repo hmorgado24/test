787bd5fe64a254557620d888427a8aa436a1a300
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-11-16 18:50:40 +0000

    asd

--- lab6.py
+++ lab6.py
@@ -1,33 +1,32 @@
 import time
-from shifter import Shifter    # extend by composition
 import random
 from led8x8 import Led8x8
 import multiprocessing
 
-def bug(dataPin, latchPin, clockPin): #lighting bug "random walking" function
-    x = random.randint(1, 8)
-    y = random.randint(0, 7)
+def bug(dataPin, latchPin, clockPin): 
+  row = random.randint(1, 8)
+  col = random.randint(0, 7)
 
-    theLEDdisplay= Led8x8(dataPin, latchPin, clockPin) #create an LED display obj that extends 8x8 class
+  theLEDdisplay= Led8x8(dataPin, latchPin, clockPin) 
 
-    while True:
-        dx = random.randint(-1, 1) #change of x by one
-        dy = random.randint(-1, 1) #change of y by one 
+  while True:
+    dx = random.randint(-1, 1) 
+    dy = random.randint(-1, 1)  
 
-        theLEDdisplay.display(x, y) #display initial random position
-        time.sleep(0.1) #requested time delay
+    theLEDdisplay.display(row, col)
+    time.sleep(0.1) 
 
-        if (x + dx < 1 or x + dx > 7): #check to make sure the bug is staying in bounds
-            x = x #if it would be "out of bounds" with the change keep it in the same place
-        else:
-            x += dx #if it won't be out of bounds then you can add the change and move by one place
+    if (row + dx < 1 or row + dx > 7): 
+      row = row 
+    else:
+      row += dx 
 
-        if (y + dy < 0 or y + dy > 7): #check to make sure the bug is staying in bounds
-            y = y #if it would be "out of bounds" with the change keep it in the same place
-        else:
-            y += dy #if it won't be out of bounds then you can add the change and move by one place
+    if (col + dy < 0 or col + dy > 7): 
+      col = col 
+    else:
+      col += dy 
 
-dataPin, latchPin, clockPin = 21, 19, 26 #assign the appropriate pins for the shift registers
+dataPin, latchPin, clockPin = 21, 19, 26 
 
-p = multiprocessing.Process(target=bug, args=(dataPin, latchPin, clockPin)) #use multiprocesssing to run the bug function continuously
-p.start() #run multiprocessing 
+p = multiprocessing.Process(target=bug, args=(dataPin, latchPin, clockPin)) 
+p.start() 
